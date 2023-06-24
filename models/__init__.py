28b56a619ba848460f3dda420b4edb2e38f96672
HeseltineBTutu <heseltinetutu@gmail.com>
2023-06-24 04:52:31 -0700

    Updated a conditional depending of the value of the environment variable HBNB_TYPE_STORAGE

--- models/__init__.py
+++ models/__init__.py
@@ -8,7 +8,7 @@
 from os import getenv
 
 # Check the value of the environment variable HBNB_TYPE_STORAGE
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
